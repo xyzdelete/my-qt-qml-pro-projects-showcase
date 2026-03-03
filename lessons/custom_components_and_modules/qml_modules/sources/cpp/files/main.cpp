be55a1fdb1377b32c3212322224786998ea817b0
Arturs <xyzdelete@protonmail.com>
2026-03-03 22:08:35 +0200

    QML Modules
    Custom Components and Modules

---
+++ lessons/custom_components_and_modules/qml_modules/sources/cpp/files/main.cpp
@@ -0,0 +1,22 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char* argv[])
+{
+  QGuiApplication app(argc, argv);
+
+  QQmlApplicationEngine engine;
+  QObject::connect(
+    &engine,
+    &QQmlApplicationEngine::objectCreationFailed,
+    &app,
+    []()
+    {
+      QCoreApplication::exit(-1);
+    },
+    Qt::QueuedConnection);
+  engine.addImportPath(":/qt/qml");
+  engine.loadFromModule("qml_module", "Main");
+
+  return app.exec();
+}
