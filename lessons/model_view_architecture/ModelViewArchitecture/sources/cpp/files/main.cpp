a2c14d1b89ec39ffd73371c13250310a860d33ef
Arturs <xyzdelete@protonmail.com>
2026-04-27 22:11:56 +0300

    ListView and ListModel
    Model View Architecture

---
+++ lessons/model_view_architecture/ModelViewArchitecture/sources/cpp/files/main.cpp
@@ -0,0 +1,24 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
+
+int main(int argc, char* argv[])
+{
+  QGuiApplication app(argc, argv);
+
+  QQmlApplicationEngine engine;
+  QObject::connect(
+    &engine,
+    &QQmlApplicationEngine::objectCreationFailed,
+    &app,
+    []()
+    {
+      QCoreApplication::exit(-1);
+    },
+    Qt::QueuedConnection);
+  engine.addImportPath(":/qt/qml");
+  QQuickStyle::setStyle("Material");
+  engine.loadFromModule("ModelViewArchitecture", "Main");
+
+  return app.exec();
+}
