6775eb2f52300c61a37467eef12a3fe63eb59b21
Arturs <xyzdelete@protonmail.com>
2026-05-17 16:00:37 +0300

    Custom Counter Type
    Non Visual Types

---
+++ lessons/non_visual_types/NonVisualTypes/sources/core/cpp/main/main.cpp
@@ -0,0 +1,40 @@
+import CppWorkerModule;
+
+#include "appwrapper.hpp"
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QQuickStyle>
+#include <QQuickWindow>
+#include <algorithm>
+
+int main(int argc, char* argv[])
+{
+  QGuiApplication app(argc, argv);
+  app.setOrganizationDomain("github.com/xyzdelete");
+  app.setOrganizationName("xyzdelete");
+  app.setApplicationName("NonVisualTypes");
+
+  CppWorkerModule::hello_world();
+
+  QQmlApplicationEngine engine;
+
+  QObject::connect(
+    &engine,
+    &QQmlApplicationEngine::objectCreationFailed,
+    &app,
+    []()
+    {
+      QCoreApplication::exit(-1);
+    },
+    Qt::QueuedConnection);
+  QStringList paths = engine.importPathList();
+  paths << ":/NonVisualTypes";
+  paths.removeDuplicates();
+  engine.setImportPathList(paths);
+  QQuickStyle::setStyle("Material");
+  engine.loadFromModule("NonVisualTypes", "Main");
+
+  return app.exec();
+}
---
+++ lessons/non_visual_types/NonVisualTypes/sources/qt/cpp/modules/RESTClientV1CppClass/sources/cpp/headers/cppclass.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QObject>
+#include <QVariantList>
+#include <QVariantMap>
+#include <QtQml/qqmlregistration.h>
+
+class CppClass : public QObject
+{
+  Q_OBJECT
+public:
+  explicit CppClass(QObject* parent = nullptr);
+
+  Q_INVOKABLE void passFromQmlToCpp(QVariantList list, QVariantMap map);
+  Q_INVOKABLE QVariantList getVariantListFromCpp();
+  Q_INVOKABLE QVariantMap getVariantMapFromCpp();
+
+  void setQmlRootObject(QObject* value);
+
+signals:
+
+public slots:
+  void triggerJSCall();
+
+private:
+  QObject* qmlRootObject;
+};
